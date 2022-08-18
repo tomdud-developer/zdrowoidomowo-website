ca2c38f72d0eec63b35169df9e323646eeff049b
tomadud-developer <tomadud045@student.polsl.pl>
2022-08-18 14:09:18 +0200

    Test commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Strona internetowa Zdrowo i Domowo
+          <h2>Strona internetowa Zdrowo i Domowo</h2>
+          Mariusz Rutkowski
         </p>
         <a
           className="App-link"
